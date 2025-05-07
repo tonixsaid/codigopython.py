853a875480ca530000e54ccf8d72af06abbf5e64
Tonix Said <tonixsaid@gmail.com>
2025-05-06 21:26:06 -0600

    Actualizar proyecto.py
    los primeros 5 puntos o cambios

--- proyecto.py
+++ proyecto.py
@@ -1,20 +1,27 @@
 import tkinter as tk
 
 def saludar():
-    nombre = entrada.get()
-    etiqueta_resultado.config(text=f"Hola {nombre}")
+    nombre = entrada_nombre.get()
+    edad = entrada_edad.get()
+    etiqueta_resultado.config(text=f"Hola {nombre}, tienes {edad} años.")
 
 ventana = tk.Tk()
-ventana.title("Saludo")
-ventana.geometry("300x150")
+ventana.title("Mi primera app gráfica")  # Cambio 5
+ventana.geometry("400x200")  # Cambio 1
 
-etiqueta = tk.Label(ventana, text="Ingresa tu nombre:")
-etiqueta.pack()
+etiqueta_nombre = tk.Label(ventana, text="Ingresa tu nombre:")
+etiqueta_nombre.pack()
 
-entrada = tk.Entry(ventana)
-entrada.pack()
+entrada_nombre = tk.Entry(ventana)
+entrada_nombre.pack()
 
-boton = tk.Button(ventana, text="Saludar", command=saludar)
+etiqueta_edad = tk.Label(ventana, text="Ingresa tu edad:")  # Cambio 3
+etiqueta_edad.pack()
+
+entrada_edad = tk.Entry(ventana)  # Cambio 3
+entrada_edad.pack()
+
+boton = tk.Button(ventana, text="Mostrar saludo", command=saludar)  # Cambio 2
 boton.pack()
 
 etiqueta_resultado = tk.Label(ventana, text="")
