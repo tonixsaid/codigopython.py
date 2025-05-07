1de9b628708d494c08a96f687d073f7b50889082
Tonix Said <tonixsaid@gmail.com>
2025-05-06 21:37:54 -0600

    Actualizar proyecto.py
    ya todos los cambios

--- proyecto.py
+++ proyecto.py
@@ -6,25 +6,29 @@ def saludar():
     etiqueta_resultado.config(text=f"Hola {nombre}, tienes {edad} años.")
 
 ventana = tk.Tk()
-ventana.title("Mi primera app gráfica")  # Cambio 5
-ventana.geometry("400x200")  # Cambio 1
+ventana.title("Mi primera app gráfica")
+ventana.geometry("400x200")
+ventana.config(bg="green")
 
-etiqueta_nombre = tk.Label(ventana, text="Ingresa tu nombre:")
+etiqueta_nombre = tk.Label(ventana, text="Ingresa tu nombre:", bg="green")
 etiqueta_nombre.pack()
 
 entrada_nombre = tk.Entry(ventana)
 entrada_nombre.pack()
 
-etiqueta_edad = tk.Label(ventana, text="Ingresa tu edad:")  # Cambio 3
+etiqueta_edad = tk.Label(ventana, text="Ingresa tu edad:", bg="green")
 etiqueta_edad.pack()
 
-entrada_edad = tk.Entry(ventana)  # Cambio 3
+entrada_edad = tk.Entry(ventana)
 entrada_edad.pack()
 
-boton = tk.Button(ventana, text="Mostrar saludo", command=saludar)  # Cambio 2
+boton = tk.Button(ventana, text="Mostrar saludo", command=saludar)
 boton.pack()
 
-etiqueta_resultado = tk.Label(ventana, text="")
+etiqueta_resultado = tk.Label(ventana, text="", bg="green")
 etiqueta_resultado.pack()
+
+etiqueta_autor = tk.Label(ventana, text="Autor: Tonix Martinez Said Emir", bg="green")
+etiqueta_autor.pack(side="bottom")
 
 ventana.mainloop()
